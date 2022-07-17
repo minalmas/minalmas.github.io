4f380c36afc03964c757df4dd4cc66931ce04b15
Almas M <melonbeam91@gmail.com>
2022-07-17 03:21:28 +0300

    Burger

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const content = document.getElementsByClassName('content')[0].getElementsByClassName('container')[0];
-const p = ['Neque totam dolore excepturi nostrum accusamus quidem, id natus soluta doloribus eligendi minima culpa repudiandae delectus quos quis sint repellat nobis.', 'Est, necessitatibus! Autem voluptates nihil fugiat!', 'Voluptatum ipsa nihil dolor sit voluptatem accusamus praesentium earum?', 'Praesentium enim quisquam deleniti, nemo velit omnis nobis amet sint esse incidunt reprehenderit quasi rerum a eos odit at obcaecati fugit harum iusto labore molestiae?', 'Aspernatur sequi culpa obcaecati nihil repellat, impedit commodi dicta molestiae molestias excepturi sit illo sint ad nesciunt maxime labore ab consectetur illum accusantium quisquam qui? Totam, nulla!', 'Doloribus fugiat iure dignissimos.'];
+const content = document.getElementsByClassName('content')[0].getElementsByClassName('container')[0]
+const p = ['Neque totam dolore excepturi nostrum accusamus quidem, id natus soluta doloribus eligendi minima culpa repudiandae delectus quos quis sint repellat nobis.', 'Est, necessitatibus! Autem voluptates nihil fugiat!', 'Voluptatum ipsa nihil dolor sit voluptatem accusamus praesentium earum?', 'Praesentium enim quisquam deleniti, nemo velit omnis nobis amet sint esse incidunt reprehenderit quasi rerum a eos odit at obcaecati fugit harum iusto labore molestiae?', 'Aspernatur sequi culpa obcaecati nihil repellat, impedit commodi dicta molestiae molestias excepturi sit illo sint ad nesciunt maxime labore ab consectetur illum accusantium quisquam qui? Totam, nulla!', 'Doloribus fugiat iure dignissimos.']
 
 document.addEventListener('DOMContentLoaded', (e) => {
 	run(p, () => {
@@ -10,8 +10,15 @@ document.addEventListener('DOMContentLoaded', (e) => {
 const run = (arr, callback) => {
 	arr.forEach((text, index) => {
 		window.setTimeout(() => {
-			content.innerHTML += `<p>${text}</p>`;
-			index == arr.length - 1 && typeof callback === 'function' && callback();
-		}, 500 * (index + 1));
-	});
+			content.innerHTML += `<p>${text}</p>`
+			index == arr.length - 1 && typeof callback === 'function' && callback()
+		}, 500 * (index + 1))
+	})
 }
+
+const burger = document.querySelector('#burger')
+const menu = document.querySelector('#menu')
+burger.addEventListener('click', e => {
+	burger.classList.toggle('active')
+	menu.classList.toggle('active')
+})
